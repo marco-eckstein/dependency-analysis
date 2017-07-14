f17a7814831b9221da935344cb435b62a696c28c
Marco Eckstein <marco.eckstein@gmx.de>
2017-07-15 00:05:05 +0200

    Refactoring

--- src/cli/dependency-analysis.ts
+++ src/cli/dependency-analysis.ts
@@ -8,19 +8,18 @@ import collapse from "../main/collapse";
 
 const version = (packageJson as any).version;
 
-const cli =
-    program
-        .usage("collapse [options] inputFile outputFile")
-        .option("collapse", "Collapse dependencies")
-        .option("-l, --levels [integer]", "If collapsing, specifies the level of collapsing. (default: 1)", 1)
-        .version(version)
-        .parse(process.argv);
+program
+    .usage("collapse [options] inputFile outputFile")
+    .option("collapse", "Collapse dependencies")
+    .option("-l, --levels [integer]", "If collapsing, specifies the level of collapsing. (default: 1)", 1)
+    .version(version)
+    .parse(process.argv);
 
-if (cli.collapse && program.args.length === 2) {
+if (program.collapse && program.args.length === 2) {
     const inputFile = program.args[0];
     const outputFile = program.args[1];
     const input = fs.readFileSync(inputFile, "utf8");
-    const collapsed = collapse(JSON.parse(input), cli.levels);
+    const collapsed = collapse(JSON.parse(input), program.levels);
     const output = JSON.stringify(collapsed, null, 2);
     fs.writeFileSync(outputFile, output, { encoding: "utf8" });
     process.exit(0);
